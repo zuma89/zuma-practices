d490b8e857ec400c423b8ee376f5492fcbb79720
zuma89 <hilaryjhho@gmail.com>
2015-09-18 14:09:00 +0800

    learning go lang

---
+++ learn_go/chapter_3/variables.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x string = "Hello World"
+	fmt.Println(x)
+	dogName := "Teddy"
+	fmt.Println("My dog's name is",dogName)
+}
+
+/* alternate way of writing this
+
+func main() {
+	var x string
+	x = "hello world"
+	fmt.Println(x)
+}
+
+*/
---
+++ learn_go/hello.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+//this is the main function
+
+func main() {
+    fmt.Printf("hello, world\n")
+    fmt.Println(len("hello world"))
+    fmt.Println("hello world"[1])
+    fmt.Println("hello " + "world")
+}
