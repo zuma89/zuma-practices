997b3a56eec89ecd84f3e8085ce48bbf8e0dc02d
zuma89 <hilaryjhho@gmail.com>
2015-09-21 22:34:29 +0800

    added chapter 6 materials

---
+++ learn_go/chapter_5/switch.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	switch i {
+		case 0: fmt.Println("Zero")
+		case 1: fmt.Println("One")
+		case 2: fmt.Println("Two")
+		case 3: fmt.Println("Three")
+		case 4: fmt.Println("Four")
+		case 5: fmt.Println("Five")
+		default: fmt.Println("Unknown")
+	}
+}
